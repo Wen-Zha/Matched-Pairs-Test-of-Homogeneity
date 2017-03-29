fa60dd10d108ff593e1638d9ebe8bab594aeb7d4
istigatrice <zhang.wenqi007@gmail.com>
2017-03-29 17:08:38 +1100

    Included new lines of Rob's code, not working
    Will work more on the charset loop

--- data play 0.3.py
+++ data play 0.3.py
@@ -1,6 +1,9 @@
 import numpy as np
 import itertools as ite
 from scipy.stats import chi2
+from Bio.Nexus import Nexus
+from Bio import AlignIO
+
 #import scipy.stats as sp
 
 def matrix(s1,s2):
@@ -46,7 +49,7 @@ def matrix(s1,s2):
             elif s2[i]=="A":
                 m[0,3]=m[0,3]+1           
         else:
-            print "error"
+            print("error")
     return m
 def MPTS(m):
     s = 0.0
@@ -57,7 +60,43 @@ def MPTS(m):
             s = s+(float(n)/float(d))
     return s
 
+def Alignment(aln_path):
+
+    dat = Nexus.Nexus()
+
+    dat.read(aln_path)
+
+    aln = AlignIO.read(open(aln_path), "nexus")
+
+    aln_array = np.array([list(rec) for rec in aln], np.character)
+
+    dat.charsets.keys() #these are the names to the CHARSETS in the .nex file, which you can iterate over in a for loop
+
+    results = {} # it would be better to use a Pandas data frame
+    for name in dat.charsets:
+        sites = dat.charsets[name]
+        # slice the alignment to get the sub-alignment for the CHARSET
+ 
+        charset_aln = aln_array[:, sites]
+
+        results[name] = MPTS_aln(charset_aln)
+    
+    return results[name]
+    
+def MPTS_aln(charset_aln):
+    # make iterator for all pairs of rows in the numpy array called charset_aln
+
+    # get list of p values where each entry is a p-value for a single pair of rows
+
+    # return that list
+    
+    pvalues={}
+    pvalues.append
+    
+    return pvalues
+
 if __name__ == '__main__': 
+    print("hello")
     s1 = "AAACTGTTCTACAGGACTGATG"
     s2 = "CATCTGTAATAGGCCACTGATA"
     
@@ -71,6 +110,9 @@ if __name__ == '__main__':
     MA = np.array([[248, 1, 1, 1],[1, 251, 1, 1],[1, 1, 252, 1],[1, 1, 1, 249]])
     #m=np.array([[248,0,0,0],[0,251,0,0],[0,0,252,0],[0,0,0,249]])
     s = MPTS(m1) 
+    print("HELLO")
     
-    print 1 - chi2.cdf(s,6.0)
+    print(1 - chi2.cdf(s,6.0))
+    
+    aln = Alignment('/Users/user/Documents/! ! 2017 ANU/Semester 1/SCNC2103/data reader/alignment.nex')
     #use itertools to loop over all sequences (itertools all pairs)
